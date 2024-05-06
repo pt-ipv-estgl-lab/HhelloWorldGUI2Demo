85613e1a8e429357e20eb5dc7ab20a7c44d5bba7
Carlos Jorge Costa <ccosta@estgl.ipv.pt>
2024-05-06 11:09:59 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Short GUI program
+
+To give an idea of the types of things that are involved on GUI development, we'll look at a short GUI program that does the same things as the previous program — open a window containing a message and an OK button, and respond to a click on the button by ending the program — but does it all by hand instead of by using the built-in `JOptionPane` class [here](https://github.com/pt-ipv-estgl-lab/GhelloWorldGUI1Demo). This also demonstrate namespaces, using `package` instruction.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/helloWorldGUI2/App.java
@@ -0,0 +1,52 @@
+package helloWorldGUI2;
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class App {
+    public static void main(String[] args) {
+
+        HelloWorldDisplay display = new HelloWorldDisplay();
+        JButton okButton = new JButton("OK");
+        ButtonHandler btnListener = new ButtonHandler();
+        okButton.addActionListener(btnListener);
+        
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        content.add(display, BorderLayout.CENTER);
+        content.add(okButton, BorderLayout.SOUTH);
+
+        JFrame mainWin = new  JFrame("GUI Test");
+        mainWin.setContentPane(content);
+
+        mainWin.setSize(250,100);
+        mainWin.setLocation(400, 300);
+        mainWin.setVisible(true);
+
+        System.out.println("The End!");
+    }
+    private static class HelloWorldDisplay  extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString("Hello World!", 20, 30);
+        }
+    }
+
+    private static class ButtonHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+        
+    }
+}
